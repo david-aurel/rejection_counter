345f2ff3027646278315b03b93ce146b39d984c5
David Durlan <david.a.durlan@gmail.com>
2020-03-09 13:36:36 +0100

    start working on connecting backend with react

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Leaderboard from './leaderboard';
+import axios from 'axios';
 
-class App extends Component {
-    state = {
-        data: null
-    };
+const App = () => {
+    const [dataState, setDataState] = useState({});
 
-    componentDidMount() {
-        // Call our fetch function below once the component mounts
-        this.callBackendAPI()
-            .then(res => this.setState({ data: res.express }))
-            .catch(err => console.log(err));
+    async function fetchData() {
+        const { data } = await axios.get('/express-backend');
+        setDataState(data);
+        console.log(dataState);
     }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    callBackendAPI = async () => {
-        const response = await fetch('/express_backend');
-        const body = await response.json();
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-        if (response.status !== 200) {
-            throw Error(body.message);
-        }
-        return body;
-    };
-
-    render() {
-        return (
-            <div className='App'>
-                <header className='App-header'>
-                    <img src={logo} className='App-logo' alt='logo' />
-                    <h1 className='App-title'>Welcome to React</h1>
-                </header>
-                <p className='App-intro'>{this.state.data}</p>
-            </div>
-        );
-    }
-}
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                <h1>Rejection Leaderboard</h1>
+                <h4>
+                    Who's got the most job offer rejections? A place to
+                    celebrate the hustle encouragement to keep going
+                </h4>
+            </header>
+        </div>
+    );
+};
 
 export default App;
+
+// Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+const callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+        throw Error(body.message);
+    }
+    return body;
+};
