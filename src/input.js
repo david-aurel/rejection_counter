40c68f54ca5a7de21208e4114d2fec592b609b3c
David Durlan <david.a.durlan@gmail.com>
2020-03-13 20:45:16 +0100

    toggle active class on update button

--- src/input.js
+++ src/input.js
@@ -3,19 +3,30 @@ import React, { useState } from 'react';
 const Input = props => {
     const [rejections, setRejections] = useState(0);
     const [name, setName] = useState('');
+    const [buttonState, setButtonState] = useState('inactive');
 
     const handleChange = e => {
         if (e.name === 'name') {
-            setName(e.value);
-            for (let el in props.leaderboard) {
-                const name = props.leaderboard[el].fields.Name;
-                if (name === e.value) {
-                    setRejections(props.leaderboard[el].fields.Rejections);
-                }
+            e.value !== ''
+                ? setButtonState('update')
+                : setButtonState('inactive');
+        }
+        for (let el in props.leaderboard) {
+            const name = props.leaderboard[el].fields.Name;
+            if (name === e.value) {
+                const currentNumber = props.leaderboard[el].fields.Rejections;
+
+                return setRejections(currentNumber);
             }
         }
+
         if (e.name === 'number') {
-            setRejections(Number(e.value));
+            return setRejections(Number(e.value));
+        }
+        if (e.innerText === '+') {
+            return setRejections(rejections + 1);
+        } else if (e.innerText === '-') {
+            return setRejections(rejections - 1);
         }
     };
     const handleClick = e => {
@@ -40,12 +51,7 @@ const Input = props => {
             props.update(name, rejections, method);
         }
     };
-    const increment = () => {
-        setRejections(rejections + 1);
-    };
-    const decrement = () => {
-        setRejections(rejections - 1);
-    };
+
     return (
         <div className='input'>
             <div className='input-fields'>
@@ -57,18 +63,18 @@ const Input = props => {
                     onChange={e => handleChange(e.target)}
                 />
                 <div className='counter'>
-                    <button onClick={() => decrement()}>-</button>
+                    <button onClick={e => handleChange(e.target)}>-</button>
                     <input
                         type='text'
                         value={rejections}
                         name='number'
                         onChange={e => handleChange(e.target)}
                     />
-                    <button onClick={() => increment()}>+</button>
+                    <button onClick={e => handleChange(e.target)}>+</button>
                 </div>
             </div>
             <button
-                className='update-button'
+                className={`input-button ${buttonState}`}
                 onClick={e => handleClick(e.target)}
             >
                 Update
